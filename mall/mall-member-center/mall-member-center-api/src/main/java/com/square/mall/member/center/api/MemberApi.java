99702a577107f54a7f32986702eb2fa5553758f4
ChenJincheng-Gencent <402634287@qq.com>
2019-08-26 00:08:26 +0800

    解决MemberApi编译报错

---
+++ mall/mall-member-center/mall-member-center-api/src/main/java/com/square/mall/member/center/api/MemberApi.java
@@ -0,0 +1,4 @@
+package com.square.mall.member.center.api;
+
+public interface MemberApi {
+}
--- mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/eo/MemberEo.java
+++ mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/eo/MemberEo.java
@@ -1,8 +1,10 @@
 package com.square.mall.member.center.service.eo;
 
 import com.juncheng.mall.common.eo.BaseEo;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * 会员基本信息
@@ -12,6 +14,8 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberEo extends BaseEo {
 
     /**
