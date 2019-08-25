9a6ca3ab6348857a58d3484d7c5ea4b6d213f653
ChenJincheng-Gencent <402634287@qq.com>
2019-08-25 11:43:36 +0800

    增加通用字段

--- mall/mall-common/src/main/java/com/juncheng/mall/common/eo/BaseEo.java
+++ mall/mall-common/src/main/java/com/juncheng/mall/common/eo/BaseEo.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * 基础EO实体类
@@ -20,9 +21,29 @@ public class BaseEo implements Serializable {
     private static final long serialVersionUID = -8752680305912189331L;
 
     /**
+     * 租户ID
+     */
+    private Long tenantId;
+
+    /**
      * 创建人
      */
     private String createPerson;
+
+    /**
+     * 更新人
+     */
+    private String updatePerson;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
 
     /**
      * 是否删除
