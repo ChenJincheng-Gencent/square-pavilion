36bc47069b03a15fd674962389a036032608a8e9
ChenJincheng-Gencent <402634287@qq.com>
2019-09-01 00:07:01 +0800

    增加Swagger配置

--- mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/eo/MemberEo.java
+++ mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/eo/MemberEo.java
@@ -1,6 +1,8 @@
 package com.square.mall.member.center.service.eo;
 
 import com.square.mall.common.eo.BaseEo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,31 +18,37 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(value = "MemberEo")
 public class MemberEo extends BaseEo {
 
     /**
      * 姓名
      */
+    @ApiModelProperty(name = "name", value = "姓名")
     private String name;
 
     /**
      * 手机号码
      */
+    @ApiModelProperty(name = "mobile", value = "手机号码")
     private String mobile;
 
     /**
      * 等级
      */
+    @ApiModelProperty(name = "level", value = "等级")
     private Integer level;
 
     /**
      * 性别
      */
+    @ApiModelProperty(name = "gender", value = "性别")
     private Integer gender;
 
     /**
      * 邮箱
      */
+    @ApiModelProperty(name = "email", value = "邮箱")
     private String email;
 
 }
