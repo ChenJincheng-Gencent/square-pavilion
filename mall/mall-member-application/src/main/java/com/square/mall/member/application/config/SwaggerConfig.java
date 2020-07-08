1ae0f67bc4d3a0fddbb243db113f70dd5deefb56
chenjincheng <15067194276@139.com>
2020-07-08 20:54:27 +0800

    swagger增加环境限制，生成环境不暴露接口

--- mall/mall-member-application/src/main/java/com/square/mall/member/application/config/SwaggerConfig.java
+++ mall/mall-member-application/src/main/java/com/square/mall/member/application/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.square.mall.member.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,6 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile({"local", "dev", "test","stage"})
 public class SwaggerConfig {
 
     @Bean
