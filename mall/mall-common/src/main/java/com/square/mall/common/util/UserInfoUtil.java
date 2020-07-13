3c46130928dfc3794f473a3fe62ab0f20fd6fb99
chenjincheng <15067194276@139.com>
2020-07-13 17:48:30 +0800

    增加用户信息工具类

---
+++ mall/mall-common/src/main/java/com/square/mall/common/util/UserInfoUtil.java
@@ -0,0 +1,32 @@
+package com.square.mall.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+/**
+ *  用户信息工具类
+ *
+ * @author Gencent
+ * @date 2020/7/13
+ */
+@Slf4j
+public class UserInfoUtil {
+
+    /**
+     *  获取用户ID
+     *
+     * @return 用户ID
+     */
+    public static Long getUserId() {
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+        return Long.parseLong(request.getHeader("userId"));
+
+    }
+
+}
