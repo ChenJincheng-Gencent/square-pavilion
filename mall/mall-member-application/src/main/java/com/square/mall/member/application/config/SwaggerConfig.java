eb5956b6484ec13a27d9272f47bd3f0382e779f9
chenjincheng <15067194276@139.com>
2020-07-15 17:52:34 +0800

    增加swagger ui增强版配置

--- mall/mall-member-application/src/main/java/com/square/mall/member/application/config/SwaggerConfig.java
+++ mall/mall-member-application/src/main/java/com/square/mall/member/application/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.square.mall.member.application.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -21,6 +24,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
 @Profile({"local", "dev", "test","stage"})
 public class SwaggerConfig {
 
@@ -28,6 +33,7 @@ public class SwaggerConfig {
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .groupName("1.0版本")
                 .select()
                 // 设置基包，只扫描这个包及其子包的接口
                 .apis(RequestHandlerSelectors.basePackage("com.square.mall"))
