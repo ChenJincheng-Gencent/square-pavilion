973fae9a3644cf34f1ce95244575f9fdd4001204
chenjincheng <15067194276@139.com>
2020-07-15 15:43:15 +0800

    增加参数校验注解

--- mall/mall-member-center/mall-member-center-api/src/main/java/com/square/mall/member/center/api/dto/MemberDto.java
+++ mall/mall-member-center/mall-member-center-api/src/main/java/com/square/mall/member/center/api/dto/MemberDto.java
@@ -6,7 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 /**
@@ -34,24 +38,28 @@ public class MemberDto implements Serializable {
      * 手机号码
      */
     @ApiModelProperty(name = "mobile", value = "手机号码")
+    @Pattern(regexp = "^1[34578][0-9]{9}$", message = "手机号必须满足格式")
     private String mobile;
 
     /**
      * 等级
      */
     @ApiModelProperty(name = "level", value = "等级", allowableValues = "1,2,3,4,5,6,7")
+    @Range(min = 1L, max = 7L, message = "等级只能是0到7间的值")
     private Integer level;
 
     /**
      * 性别
      */
     @ApiModelProperty(name = "gender", value = "性别", allowableValues = "0,1,2")
+    @Range(max = 2L, message = "性别只能是0到2间的值")
     private Integer gender;
 
     /**
      * 邮箱
      */
     @ApiModelProperty(name = "email", value = "邮箱")
+    @Email(message = "邮箱必须满足格式")
     private String email;
 
 }
