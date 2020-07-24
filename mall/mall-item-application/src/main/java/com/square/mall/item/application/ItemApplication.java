8eb418dfd51fed2a218e0a76c288435f7c3605b8
chenjincheng <15067194276@139.com>
2020-07-24 18:39:29 +0800

    增加商品应用

---
+++ mall/mall-item-application/src/main/java/com/square/mall/item/application/ItemApplication.java
@@ -0,0 +1,21 @@
+package com.square.mall.item.application;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 商品应用服务启动类
+ *
+ * @author Gencent
+ * @date 2020/7/24
+ */
+
+@SpringBootApplication
+@EnableDubbo
+public class ItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
+}
