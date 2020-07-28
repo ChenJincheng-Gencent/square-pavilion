e609201468632763f0f9788a37b6bfeaafe94488
chenjincheng <15067194276@139.com>
2020-07-28 11:41:21 +0800

    增加规格选项查询功能

---
+++ mall/mall-item-center/mall-item-center-service/src/main/java/com/square/mall/item/center/service/apiimpl/query/SpecificationOptionQueryApiImpl.java
@@ -0,0 +1,31 @@
+package com.square.mall.item.center.service.apiimpl.query;
+
+import com.square.mall.common.dto.RspDto;
+import com.square.mall.item.center.api.dto.SpecificationOptionDto;
+import com.square.mall.item.center.api.query.SpecificationOptionQueryApi;
+import com.square.mall.item.center.service.service.SpecificationOptionService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 规格选项查询API实现类
+ *
+ * @author Gencent
+ * @date 2020/7/28
+ */
+@Slf4j
+@Service
+public class SpecificationOptionQueryApiImpl implements SpecificationOptionQueryApi {
+
+    @Resource
+    private SpecificationOptionService specificationOptionService;
+
+    @Override
+    public RspDto<List<SpecificationOptionDto>> selectSpecificationOptionBySpecId(Long specId) {
+        return new RspDto<>(specificationOptionService.selectSpecificationOptionBySpecId(specId));
+    }
+
+}
