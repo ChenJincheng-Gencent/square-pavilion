b267119e0af142166971ff77d2633bd7f32d35e5
chenjincheng <15067194276@139.com>
2020-07-28 21:01:20 +0800

    增加分类功能

---
+++ mall/mall-item-center/mall-item-center-service/src/main/java/com/square/mall/item/center/service/dao/CategoryDao.java
@@ -0,0 +1,65 @@
+package com.square.mall.item.center.service.dao;
+
+import com.square.mall.item.center.service.eo.BrandEo;
+import com.square.mall.item.center.service.eo.CategoryEo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 品牌Dao
+ *
+ * @author Gencent
+ * @date 2020/7/24
+ */
+public interface CategoryDao {
+
+    /**
+     * 插入品牌
+     *
+     * @param brandEo 品牌
+     * @return 是否成功，1成功，0失败
+     */
+    int insertBrand(BrandEo brandEo);
+
+    /**
+     * 更新品牌
+     *
+     * @param brandEo 品牌
+     * @return 是否成功，1成功，0失败
+     */
+    int updateBrand(BrandEo brandEo);
+
+    /**
+     * 删除品牌
+     *
+     * @param id 数据库ID
+     * @return 是否成功，1成功，0失败
+     */
+    int deleteBrand(@Param("id") Long id);
+
+    /**
+     * 条件查询品牌列表
+     *
+     * @param brandEo 查询条件
+     * @return 品牌数据列表
+     */
+    List<BrandEo> selectBrandByCondition(BrandEo brandEo);
+
+    /**
+     * 根据名称查询品牌
+     *
+     * @param name 名称
+     * @return 品牌
+     */
+    BrandEo selectBrandByName(@Param("name") String name);
+
+    /**
+     * 根据上级ID查询分类
+     *
+     * @param parentId 上级ID
+     * @return 分类
+     */
+    CategoryEo selectCategoryByParentId(@Param("parentId") Long parentId);
+
+}
