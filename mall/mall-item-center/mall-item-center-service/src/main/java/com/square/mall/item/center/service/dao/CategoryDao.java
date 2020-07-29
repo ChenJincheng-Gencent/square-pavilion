398065a06c45684a011eaa3483bc9d7a0711f6d1
chenjincheng <15067194276@139.com>
2020-07-29 09:40:35 +0800

    增加分类功能

--- mall/mall-item-center/mall-item-center-service/src/main/java/com/square/mall/item/center/service/dao/CategoryDao.java
+++ mall/mall-item-center/mall-item-center-service/src/main/java/com/square/mall/item/center/service/dao/CategoryDao.java
@@ -1,58 +1,40 @@
 package com.square.mall.item.center.service.dao;
 
-import com.square.mall.item.center.service.eo.BrandEo;
 import com.square.mall.item.center.service.eo.CategoryEo;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
-
 /**
- * 品牌Dao
+ * 分类Dao
  *
  * @author Gencent
- * @date 2020/7/24
+ * @date 2020/7/28
  */
 public interface CategoryDao {
 
     /**
-     * 插入品牌
+     * 插入分类
      *
-     * @param brandEo 品牌
+     * @param categoryEo 分类
      * @return 是否成功，1成功，0失败
      */
-    int insertBrand(BrandEo brandEo);
+    int insertCategory(CategoryEo categoryEo);
 
     /**
-     * 更新品牌
+     * 根据上级ID更新分类
      *
-     * @param brandEo 品牌
+     * @param categoryEo 分类
      * @return 是否成功，1成功，0失败
      */
-    int updateBrand(BrandEo brandEo);
+    int updateCategoryByParentId(CategoryEo categoryEo);
 
     /**
-     * 删除品牌
+     * 删除分类
      *
      * @param id 数据库ID
      * @return 是否成功，1成功，0失败
      */
-    int deleteBrand(@Param("id") Long id);
+    int deleteCategory(@Param("id") Long id);
 
-    /**
-     * 条件查询品牌列表
-     *
-     * @param brandEo 查询条件
-     * @return 品牌数据列表
-     */
-    List<BrandEo> selectBrandByCondition(BrandEo brandEo);
-
-    /**
-     * 根据名称查询品牌
-     *
-     * @param name 名称
-     * @return 品牌
-     */
-    BrandEo selectBrandByName(@Param("name") String name);
 
     /**
      * 根据上级ID查询分类
