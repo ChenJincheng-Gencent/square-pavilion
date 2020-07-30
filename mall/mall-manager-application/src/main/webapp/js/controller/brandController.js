60974903f53d9cbf979c5bf7a414600938a58b65
chenjincheng <15067194276@139.com>
2020-07-30 20:16:21 +0800

    修复查询不显示

--- mall/mall-manager-application/src/main/webapp/js/controller/brandController.js
+++ mall/mall-manager-application/src/main/webapp/js/controller/brandController.js
@@ -26,7 +26,7 @@ app.controller('brandController' ,function($scope,$controller   ,brandService){
 	$scope.findOne=function(id){				
 		brandService.findOne(id).success(
 			function(response){
-				$scope.entity= response;					
+				$scope.entity= response.data;
 			}
 		);				
 	}
