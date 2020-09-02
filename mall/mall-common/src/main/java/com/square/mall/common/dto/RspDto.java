373152f91e932cdd9c881c66886d524305719c1d
chenjincheng <15067194276@139.com>
2020-09-02 18:02:22 +0800

    代码优化

--- mall/mall-common/src/main/java/com/square/mall/common/dto/RspDto.java
+++ mall/mall-common/src/main/java/com/square/mall/common/dto/RspDto.java
@@ -24,12 +24,12 @@ public class RspDto<T> implements Serializable {
     /**
      * 成功
      */
-    public static final RspDto SUCCESS = new RspDto(ErrorCode.SUCCESS);
+    public static final RspDto<Void> SUCCESS = new RspDto<>(ErrorCode.SUCCESS);
 
     /**
      * 失败
      */
-    public static final RspDto FAILED = new RspDto(ErrorCode.FAILED);
+    public static final RspDto<Void> FAILED = new RspDto<>(ErrorCode.FAILED);
 
     /**
      * 错误码
