88d0dd29d256b55953549a1ee926a1ee813f2aaa
chenjincheng <15067194276@139.com>
2020-10-22 17:34:13 +0800

    增加服务发现注解

--- mall/mall-manager-application/src/main/java/com/square/mall/manager/application/ManagerApplication.java
+++ mall/mall-manager-application/src/main/java/com/square/mall/manager/application/ManagerApplication.java
@@ -3,6 +3,7 @@ package com.square.mall.manager.application;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 管理应用服务启动类
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDubbo
+@EnableDiscoveryClient
 public class ManagerApplication {
 
     public static void main(String[] args) {
