9b5addf6f12e17a5db76a159309c434fb1fac335
chenjincheng <15067194276@139.com>
2020-10-22 17:55:00 +0800

    增加feign注解

--- mall/mall-member-application/src/main/java/com/square/mall/member/application/MemberApplication.java
+++ mall/mall-member-application/src/main/java/com/square/mall/member/application/MemberApplication.java
@@ -5,6 +5,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 会员应用服务启动类
@@ -16,6 +17,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDubbo
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MemberApplication {
 
     public static void main(String[] args) {
