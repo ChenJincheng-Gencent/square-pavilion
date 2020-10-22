d6a9c1c0f5aca3583e6f006b836c87e98de7f2fe
chenjincheng <15067194276@139.com>
2020-10-22 17:53:38 +0800

    增加feign注解

--- mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/MemberCenterApplication.java
+++ mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/MemberCenterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 会员中心服务启动类
@@ -17,6 +18,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
 @EnableDubbo
 @EnableDiscoveryClient
+@EnableFeignClients
 public class MemberCenterApplication {
 
     public static void main(String[] args) {
