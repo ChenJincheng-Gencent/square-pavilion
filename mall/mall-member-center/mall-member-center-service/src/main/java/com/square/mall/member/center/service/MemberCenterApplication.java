5a574deeeb8995b33127fea485788ac654360c3a
chenjincheng <15067194276@139.com>
2020-10-22 17:29:14 +0800

    增加服务发现注解

--- mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/MemberCenterApplication.java
+++ mall/mall-member-center/mall-member-center-service/src/main/java/com/square/mall/member/center/service/MemberCenterApplication.java
@@ -5,6 +5,7 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 会员中心服务启动类
@@ -15,6 +16,7 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
 @EnableDubbo
+@EnableDiscoveryClient
 public class MemberCenterApplication {
 
     public static void main(String[] args) {
