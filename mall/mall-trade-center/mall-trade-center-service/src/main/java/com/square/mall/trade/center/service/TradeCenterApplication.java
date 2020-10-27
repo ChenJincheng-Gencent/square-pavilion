41db0a1752008ef26c1b5de95a18d667e401b428
chenjincheng <15067194276@139.com>
2020-10-27 19:23:07 +0800

    增加注解

--- mall/mall-trade-center/mall-trade-center-service/src/main/java/com/square/mall/trade/center/service/TradeCenterApplication.java
+++ mall/mall-trade-center/mall-trade-center-service/src/main/java/com/square/mall/trade/center/service/TradeCenterApplication.java
@@ -1,9 +1,10 @@
 package com.square.mall.trade.center.service;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 交易中心服务启动类
@@ -13,7 +14,8 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
  */
 
 @SpringBootApplication(exclude = MongoAutoConfiguration.class)
-@EnableDubbo
+@EnableDiscoveryClient
+@EnableFeignClients
 public class TradeCenterApplication {
 
     public static void main(String[] args) {
