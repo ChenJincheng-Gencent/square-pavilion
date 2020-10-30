a94b2cf132fb45f9ca6f69160de1fe939ce7b018
chenjincheng <15067194276@139.com>
2020-10-30 16:20:25 +0800

    增加枚举类

---
+++ mall/mall-common/src/main/java/com/square/mall/common/util/item/OnShelfStatus.java
@@ -0,0 +1,42 @@
+package com.square.mall.common.util.item;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 上下架状态
+ *
+ * @author Gencent
+ * @date 2020/10/30
+ */
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum OnShelfStatus {
+
+    /**
+     * 下架
+     */
+    OFF_SHELF(0, "下架"),
+
+    /**
+     * 上架
+     */
+    ON_SHELF(1, "上架"),
+
+    /**
+     * 隐藏
+     */
+    HIDE(2, "隐藏");
+
+    /**
+     * 枚举值
+     */
+    private Integer value;
+
+    /**
+     * 描述
+     */
+    private String desc;
+
+}
---
+++ mall/mall-common/src/main/java/com/square/mall/common/util/trade/OrderStatus.java
@@ -0,0 +1,57 @@
+package com.square.mall.common.util.trade;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 订单状态
+ *
+ * @author Gencent
+ * @date 2020/10/30
+ */
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum OrderStatus {
+
+    /**
+     * 待支付
+     */
+    UNPAID(0, "待支付"),
+
+    /**
+     * 已支付
+     */
+    PAID(1, "已支付"),
+
+    /**
+     * 已失效
+     */
+    INVALID(2, "已失效"),
+
+    /**
+     * 已完成
+     */
+    COMPLETED(3, "已完成"),
+
+    /**
+     * 已取消
+     */
+    CANCELED(4, "已取消"),
+
+    /**
+     * 已删除
+     */
+    DELETED(5, "已删除");
+
+    /**
+     * 枚举值
+     */
+    private Integer value;
+
+    /**
+     * 描述
+     */
+    private String desc;
+
+}
