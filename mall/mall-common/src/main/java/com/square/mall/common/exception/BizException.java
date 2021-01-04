096dbdd722ae3ce50909e7745128cebe931b4367
chenjincheng <15067194276@139.com>
2021-01-04 20:41:37 +0800

    增加业务异常类

---
+++ mall/mall-common/src/main/java/com/square/mall/common/exception/BizException.java
@@ -0,0 +1,52 @@
+package com.square.mall.common.exception;
+
+import com.square.mall.common.util.ErrorCode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 业务异常
+ *
+ * @author Gencent
+ * @date 2020/1/4
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BizException extends RuntimeException {
+
+    private static final long serialVersionUID = 6147256750452161408L;
+
+    private String code;
+
+    private String msg;
+
+
+    public BizException(Throwable cause) {
+        super(cause);
+    }
+
+    public BizException(String message) {
+        super(message);
+        this.msg = message;
+    }
+
+    public BizException(String message, Throwable cause) {
+        super(message, cause);
+        this.msg = message;
+    }
+
+    public BizException(ErrorCode errorCode) {
+        super(errorCode.getMsg());
+        this.msg = errorCode.getMsg();
+        this.code = errorCode.getCode();
+    }
+
+    public BizException(String code, String msg) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
+
+}
+
