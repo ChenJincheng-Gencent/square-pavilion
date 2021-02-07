49093b0be78d32f3f8fa2f55eb1fd36dab489370
chenjincheng <15067194276@139.com>
2021-02-07 14:17:54 +0800

    修改注释

--- mall/mall-share-application/src/main/java/com/square/mall/share/application/ShareApplication.java
+++ mall/mall-share-application/src/main/java/com/square/mall/share/application/ShareApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 商品应用服务启动类
+ * 分享应用服务启动类
  *
  * @author Gencent
  * @date 2020/7/24
