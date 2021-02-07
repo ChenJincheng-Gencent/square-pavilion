81590f8792ad76b61ee29aaa2c579c4b4dee8548
chenjincheng <15067194276@139.com>
2021-02-07 14:50:29 +0800

    增加定时任务应用

---
+++ mall/mall-job-application/src/main/java/com/square/mall/job/application/JobApplication.java
@@ -0,0 +1,23 @@
+package com.square.mall.share.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 定时任务应用服务启动类
+ *
+ * @author Gencent
+ * @date 2021/2/7
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareApplication.class, args);
+    }
+}
