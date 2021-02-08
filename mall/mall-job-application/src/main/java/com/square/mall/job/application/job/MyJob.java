7d47572d795679af35cab68815d74019aa28c27e
ChenJincheng-Gencent <402634287@qq.com>
2021-02-08 21:56:08 +0800

    代码优化

--- mall/mall-job-application/src/main/java/com/square/mall/job/application/job/MyJob.java
+++ mall/mall-job-application/src/main/java/com/square/mall/job/application/job/MyJob.java
@@ -10,8 +10,6 @@ import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,24 +28,11 @@ public class MyJob implements SimpleJob {
 
     @Override
     public void execute(ShardingContext context) {
-        switch (context.getShardingItem()) {
-            case 0:
-                // do something by sharding item 0
-                break;
-            case 1:
-                // do something by sharding item 1
-                break;
-            case 2:
-                // do something by sharding item 2
-                break;
-            // case n: ...
-        }
-        log.info(String.format("Item777777777777: %s | Time: %s | Thread: %s | %s",
-                context.getShardingItem(), new SimpleDateFormat("HH:mm:ss").format(new Date()), Thread
-                    .currentThread().getId(), "SIMPLE"));
 
+        log.info("MyJob 定时任务执行开始");
         RspDto<List<BrandDto>> brandRspDto = brandApi.selectBrandAll();
         log.info("brandDtoList: {}", brandRspDto.getData());
+        log.info("MyJob 定时任务执行结束");
 
     }
 }
