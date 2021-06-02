f0c033946fbb82c1b495341d51f60b26a3504d5c
ChenJincheng-Gencent <402634287@qq.com>
2021-06-02 23:10:44 +0800

    增加swagger注解

--- mall/mall-trade-application/src/main/java/com/square/mall/trade/application/controller/ShoppingCartController.java
+++ mall/mall-trade-application/src/main/java/com/square/mall/trade/application/controller/ShoppingCartController.java
@@ -3,6 +3,8 @@ package com.square.mall.trade.application.controller;
 import com.square.mall.common.dto.CommonRes;
 import com.square.mall.trade.application.service.ShoppingCartService;
 import com.square.mall.trade.center.api.dto.ShoppingCartDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/shopping/cart")
 @Slf4j
+@Api(tags = "购物车")
 public class ShoppingCartController {
 
     @Resource
@@ -29,6 +32,7 @@ public class ShoppingCartController {
      * @param shoppingCartDto 购物车
      * @return 响应
      */
+    @ApiOperation("加入购物车")
     @PostMapping("/addShoppingCart")
     public CommonRes<Void> addShoppingCart(@RequestBody ShoppingCartDto shoppingCartDto) {
         return shoppingCartService.addShoppingCart(shoppingCartDto);
@@ -40,6 +44,7 @@ public class ShoppingCartController {
      * @param shoppingCartDto 购物车
      * @return 响应
      */
+    @ApiOperation("更新购物车")
     @PutMapping("/updateShoppingCart")
     public CommonRes<Void> updateShoppingCart(@RequestBody ShoppingCartDto shoppingCartDto) {
 
@@ -53,6 +58,7 @@ public class ShoppingCartController {
      * @param memberId 会员ID
      * @return 购物车列表
      */
+    @ApiOperation("根据会员ID查询购物车列表")
     @GetMapping("/getShoppingCartList")
     public CommonRes<List<ShoppingCartDto>> getShoppingCartList(@RequestParam("memberId") Long memberId) {
         return shoppingCartService.getShoppingCartList(memberId);
@@ -65,8 +71,10 @@ public class ShoppingCartController {
      * @param itemId 商品ID
      * @return 响应
      */
+    @ApiOperation("根据会员ID和商品ID删除购物车")
     @DeleteMapping("/deleteShoppingCart")
-    public CommonRes<Void> deleteShoppingCart(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId) {
+    public CommonRes<Void> deleteShoppingCart(@RequestParam("memberId") Long memberId,
+                                              @RequestParam("itemId") Long itemId) {
         return shoppingCartService.deleteShoppingCart(memberId, itemId);
     }
 
@@ -77,19 +85,22 @@ public class ShoppingCartController {
      * @param itemIds 商品ID列表
      * @return 响应
      */
-    @DeleteMapping("/batch")
-    public CommonRes batchDeleteShoppingCartList(@RequestParam("memberId") Long memberId, @RequestParam("itemIds") Long[] itemIds) {
+    @ApiOperation("根据会员ID和商品ID列表批量删除购物车")
+    @DeleteMapping("/batchDeleteShoppingCartList")
+    public CommonRes<Void> batchDeleteShoppingCartList(@RequestParam("memberId") Long memberId,
+                                                       @RequestParam("itemIds") Long[] itemIds) {
         return shoppingCartService.batchDeleteShoppingCartList(memberId, itemIds);
     }
 
     /**
-     * 根据会员ID和商品ID列表批量删除购物车
+     * 根据会员ID删除所有购物车列表
      *
      * @param memberId 会员ID
      * @return 响应
      */
-    @DeleteMapping("/all")
-    public CommonRes deleteAllShoppingCartList(@RequestParam("memberId") Long memberId) {
+    @ApiOperation("根据会员ID删除所有购物车列表")
+    @DeleteMapping("/deleteAllShoppingCartList")
+    public CommonRes<Void> deleteAllShoppingCartList(@RequestParam("memberId") Long memberId) {
         return shoppingCartService.deleteAllShoppingCartList(memberId);
     }
 
