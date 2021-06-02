3baad76e707ca4cddd925e0a8c646b537c7105e8
chenjincheng <15067194276@139.com>
2021-06-02 15:33:10 +0800

    新增网关工程

---
+++ mall/mall-gateway/src/main/java/com/square/mall/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.square.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关服务启动类
+ *
+ * @author Gencent
+ * @date 2020/7/22
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
