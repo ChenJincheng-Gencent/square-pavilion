4f48027d321adb85bc13f0a60c5b64d39208675b
chenjincheng <15067194276@139.com>
2021-06-02 15:50:50 +0800

    修改swagger配置

--- mall/mall-trade-application/src/main/java/com/square/mall/trade/application/config/SwaggerConfig.java
+++ mall/mall-trade-application/src/main/java/com/square/mall/trade/application/config/SwaggerConfig.java
@@ -1,54 +1,91 @@
+
 package com.square.mall.trade.application.config;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import com.github.xiaoymin.knife4j.core.util.CollectionUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.List;
 
 /**
- *  Swagger配置
+ * Swagger配置
  *
  * @author Gencent
- * @date 2019/8/31
+ * @date 2020/7/20
  */
-
 @Configuration
-@EnableSwagger2
-@EnableKnife4j
+@EnableSwagger2WebMvc
 @Import(BeanValidatorPluginsConfiguration.class)
 @Profile({"local", "dev", "test","stage"})
 public class SwaggerConfig {
 
-    @Bean
-    public Docket createRestApi() {
+    @Bean(value = "tradeApplication")
+    @Order(value = 1)
+    public Docket groupRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .groupName("1.0版本")
+                .apiInfo(groupApiInfo())
                 .select()
-                // 设置基包，只扫描这个包及其子包的接口
-                .apis(RequestHandlerSelectors.basePackage("com.square.mall"))
+                .apis(RequestHandlerSelectors.basePackage("com.xiaominfo.swagger.service.order.controller"))
                 .paths(PathSelectors.any())
+
+                .build().securityContexts(CollectionUtils.newArrayList(securityContext(),securityContext1())).securitySchemes(CollectionUtils.<SecurityScheme>newArrayList(apiKey(),apiKey1()));
+    }
+
+    private ApiInfo groupApiInfo(){
+        return new ApiInfoBuilder()
+                .title("swagger-bootstrap-ui很棒~~~！！！")
+                .description("<div style='font-size:14px;color:red;'>swagger-bootstrap-ui-demo RESTful APIs</div>")
+                .termsOfServiceUrl("http://www.group.com/")
+                .contact("group@qq.com")
+                .version("1.0")
                 .build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Square pavilion trade application")
-                .description("本项目用于提供交易相关业务的应用层功能")
-                .contact(new Contact("Gencent", "https://github.com/ChenJincheng-Gencent",
-                    "402634287@qq.com"))
-                .version("1.0.0")
+
+
+    private ApiKey apiKey() {
+        return new ApiKey("BearerToken", "Authorization", "header");
+    }
+    private ApiKey apiKey1() {
+        return new ApiKey("BearerToken1", "Authorization-x", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
                 .build();
+    }
+    private SecurityContext securityContext1() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth1())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return CollectionUtils.newArrayList(new SecurityReference("BearerToken", authorizationScopes));
+    }
+    List<SecurityReference> defaultAuth1() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return CollectionUtils.newArrayList(new SecurityReference("BearerToken1", authorizationScopes));
     }
 
 }
