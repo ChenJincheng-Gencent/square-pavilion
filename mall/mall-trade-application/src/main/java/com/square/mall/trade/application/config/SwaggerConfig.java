f62e5bbd9ec5051c5b6e2490d5909fc28c8243c7
ChenJincheng-Gencent <402634287@qq.com>
2021-06-04 00:58:33 +0800

    更改配置信息

--- mall/mall-trade-application/src/main/java/com/square/mall/trade/application/config/SwaggerConfig.java
+++ mall/mall-trade-application/src/main/java/com/square/mall/trade/application/config/SwaggerConfig.java
@@ -11,13 +11,11 @@ import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
-import java.util.List;
 
 /**
  * Swagger配置
@@ -40,70 +38,19 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.square.mall.trade.application"))
                 .paths(PathSelectors.any())
                 .build()
-                .securityContexts(CollectionUtils.newArrayList(tokenSecurityContext(), ipSecurityContext(), userIdSecurityContext()))
-                .securitySchemes(CollectionUtils.<SecurityScheme>newArrayList(token(), ip(), userId()));
+                .securityContexts(CollectionUtils.newArrayList())
+                .securitySchemes(CollectionUtils.newArrayList());
     }
 
     private ApiInfo groupApiInfo(){
         return new ApiInfoBuilder()
-                .title("Square Pavilion Mall Trade Application")
+                .title("四方阁交易应用")
                 .description("本项目用于提供交易相关业务的应用层功能")
-                .termsOfServiceUrl("localhost:6100")
+                .termsOfServiceUrl("192.168.31.10:9100")
                 .contact(new Contact("Gencent", "https://github.com/ChenJincheng-Gencent",
                     "402634287@qq.com"))
                 .version("1.0")
                 .build();
-    }
-
-
-
-    private ApiKey token() {
-        return new ApiKey("Token", "Token", "header");
-    }
-    private ApiKey ip() {
-        return new ApiKey("Ip", "Ip", "header");
-    }
-    private ApiKey userId() {
-        return new ApiKey("UserId", "UserId", "header");
-    }
-
-    private SecurityContext tokenSecurityContext() {
-        return SecurityContext.builder()
-                .securityReferences(tokenAuth())
-                .forPaths(PathSelectors.regex("/.*"))
-                .build();
-    }
-    private SecurityContext ipSecurityContext() {
-        return SecurityContext.builder()
-                .securityReferences(ipAuth())
-                .forPaths(PathSelectors.regex("/.*"))
-                .build();
-    }
-
-    private SecurityContext userIdSecurityContext() {
-        return SecurityContext.builder()
-                .securityReferences(userIdAuth())
-                .forPaths(PathSelectors.regex("/.*"))
-                .build();
-    }
-
-    List<SecurityReference> tokenAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return CollectionUtils.newArrayList(new SecurityReference("Token", authorizationScopes));
-    }
-    List<SecurityReference> ipAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return CollectionUtils.newArrayList(new SecurityReference("Ip", authorizationScopes));
-    }
-    List<SecurityReference> userIdAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return CollectionUtils.newArrayList(new SecurityReference("UserId", authorizationScopes));
     }
 
 }
