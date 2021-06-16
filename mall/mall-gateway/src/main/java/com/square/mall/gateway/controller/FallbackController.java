3822b240aadd61f5479ef0cad4ac2b0af706226e
ChenJincheng-Gencent <402634287@qq.com>
2021-06-16 17:15:31 +0800

    增加降级策略注释

--- mall/mall-gateway/src/main/java/com/square/mall/gateway/controller/FallbackController.java
+++ mall/mall-gateway/src/main/java/com/square/mall/gateway/controller/FallbackController.java
@@ -6,6 +6,12 @@ import com.square.mall.common.dto.CommonRes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 熔断降级返回的接口
+ *
+ * @author Gencent
+ * @date 2021/6/8
+ */
 @RestController
 public class FallbackController {
 
